4a26132a168ca222fbb16b22a17d13b7973db5b5
Adhi Hargo <cadmus.sw@gmail.com>
2014-05-04 21:14:52 +0700

    Readjust window positioning, reorder window rules, add keyboard shortcut command lines.

--- home/.config/devilspie2/window.lua
+++ home/.config/devilspie2/window.lua
@@ -1,30 +1,33 @@
 -- Left
-if(get_window_class() == "Mate-terminal" or
-   get_window_class() == "Emacs23" or
-   get_window_class() == "Caja") then
+if (	get_window_class() == "Mate-terminal" or
+	get_window_class() == "Emacs23" or
+	get_window_class() == "Pluma" or
+	get_window_class() == "Caja") then
    set_window_geometry2(0, 0, 505, 750)
    maximize_vertically()
-end
 
 -- Right
-if(get_window_class() == "Vlc" or
-   get_window_class() == "Gthumb" or
-   get_window_class() == "Firefox") then
-   set_window_geometry2(506, 0, 775, 750)
+elseif (get_window_class() == "Vlc" or
+	get_window_class() == "Gthumb" or
+	get_window_class() == "Chromium-browser" or
+	get_window_class() == "Firefox") then
+   set_window_geometry2(516, 0, 765, 750)
    maximize_vertically()
-end
 
 -- Workspace Reading
-if(get_window_class() == "Atril") then
+elseif (get_window_class() == "Atril") then
    set_window_workspace(3)
-end
+   maximize()
 
 -- Workspace Drawing
-if(get_window_class() == "Krita" or
-   string.find(get_window_class(), "Gimp")) then
+elseif (get_window_class() == "Krita" or
+	string.find(get_window_class(), "Gimp")) then
    set_window_workspace(4)
-end
 
-if(get_window_class() == "Blender") then
-   maximize();
+elseif (get_window_class() == "Blender") then
+   if (get_window_name() == "Blender User Preferences") then
+      set_window_geometry2(516, 0, 765, 721)
+   else
+      maximize();
+   end
 end
