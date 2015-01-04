89a3b17d08446b053c3acb2f07a0c3082f0609a5
Adhi Hargo <cadmus.sw@gmail.com>
2015-01-04 07:13:05 +0700

    Devilspie2: Update window name matching and wider res.

--- home/.config/devilspie2/window.lua
+++ home/.config/devilspie2/window.lua
@@ -1,9 +1,11 @@
 -- Left
-if (	get_window_class() == "Mate-terminal" or
-	get_window_class() == "Emacs23" or
+if (	string.match(get_window_class(), "-terminal$") or
+	string.match(get_window_class(), "^Emacs") or
+	get_window_class() == "Gedit" or
 	get_window_class() == "Pluma" or
+	get_window_class() == "Nemo" or
 	get_window_class() == "Caja") then
-   set_window_geometry2(0, 0, 505, 750)
+   set_window_geometry2(0, 0, 682, 722)
    maximize_vertically()
 
 -- Right
@@ -11,11 +13,12 @@ elseif (get_window_class() == "Vlc" or
 	get_window_class() == "Gthumb" or
 	get_window_class() == "Chromium-browser" or
 	get_window_class() == "Firefox") then
-   set_window_geometry2(516, 0, 765, 750)
+   set_window_geometry2(694, 0, 682, 722)
    maximize_vertically()
 
 -- Workspace Reading
-elseif (get_window_class() == "Atril") then
+elseif (get_window_class() == "Atril" or
+        get_window_class() == "Evince") then
    set_window_workspace(3)
    maximize()
 
@@ -25,9 +28,10 @@ elseif (get_window_class() == "Krita" or
    set_window_workspace(4)
 
 elseif (get_window_class() == "Blender") then
-   if (get_window_name() == "Blender User Preferences") then
-      set_window_geometry2(516, 0, 765, 721)
+   if (string.find(get_window_name(), "Blender User Preferences")) then
+      set_window_geometry2(694, 0, 682, 722)
    else
       maximize();
    end
+   set_window_workspace(2)
 end
